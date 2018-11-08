b3aba880c97202fa869d233d613678df9d601473
Alec Winograd <awinograd@gmail.com>
2018-11-08 10:37:53 -0600

    Use children instead of iframe

--- src/content-frame.js
+++ src/content-frame.js
@@ -2,12 +2,6 @@ import React from 'react';
 
 export default class ContentFrame extends React.Component {
   render() {
-    return (
-      <iframe
-        id={this.props.id}
-        name={this.props.name}
-        src={this.props.src}
-      />
-    );
+    return this.props.children;
   }
 }
